9ad01e39548c519d6cb740d5a2892f6f6cb020e6
matteobonina96 <68783037+matteobonina96@users.noreply.github.com>
2020-10-29 16:17:23 +0100

    Update ProfessorDTO.java

--- src/main/java/it/polito/ai/laboratorio3/dtos/ProfessorDTO.java
+++ src/main/java/it/polito/ai/laboratorio3/dtos/ProfessorDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProfessorDTO {
+public class ProfessorDTO extends RepresentationModel<ProfessorDTO> {
     private String id;
     private String name;
     private String firstName;

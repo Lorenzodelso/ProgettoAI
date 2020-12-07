0f649d3de1611be518db119e1dc10a756373ad89
matteobonina96 <68783037+matteobonina96@users.noreply.github.com>
2020-12-07 23:46:57 +0100

    Update EssayRepository.java
    aggiunta @Repository

--- src/main/java/it/polito/ai/laboratorio3/repositories/EssayRepository.java
+++ src/main/java/it/polito/ai/laboratorio3/repositories/EssayRepository.java
@@ -2,6 +2,8 @@ package it.polito.ai.laboratorio3.repositories;
 
 import it.polito.ai.laboratorio3.entities.Essay;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EssayRepository extends JpaRepository<Essay, Long> {
 }

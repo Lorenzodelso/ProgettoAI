9ff0fedc2a157eb0c7ebedd692435ff4eba32acf
matteobonina96 <68783037+matteobonina96@users.noreply.github.com>
2020-12-07 23:47:42 +0100

    Update RegistrationTokenRepository.java
    aggiunta @Repository

--- src/main/java/it/polito/ai/laboratorio3/repositories/RegistrationTokenRepository.java
+++ src/main/java/it/polito/ai/laboratorio3/repositories/RegistrationTokenRepository.java
@@ -2,6 +2,8 @@ package it.polito.ai.laboratorio3.repositories;
 
 import it.polito.ai.laboratorio3.entities.RegistrationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RegistrationTokenRepository extends JpaRepository<RegistrationToken,String> {
 }

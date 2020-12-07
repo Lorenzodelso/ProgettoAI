7398f9a3a3995c3311f23a01076685755c8712ac
matteobonina96 <68783037+matteobonina96@users.noreply.github.com>
2020-12-07 23:51:35 +0100

    Update TokenNotFoundException.java
    correzione messaggio exception

--- src/main/java/it/polito/ai/laboratorio3/exceptions/TokenNotFoundException.java
+++ src/main/java/it/polito/ai/laboratorio3/exceptions/TokenNotFoundException.java
@@ -2,6 +2,6 @@ package it.polito.ai.laboratorio3.exceptions;
 
 public class TokenNotFoundException extends TeamServiceException {
     public TokenNotFoundException(){
-        super("Course not enabled!");
+        super("Token not found!");
     }
 }

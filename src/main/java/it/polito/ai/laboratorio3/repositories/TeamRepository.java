fd9d20713cb5123007e02597f108c235e5172ae2
matteobonina96 <68783037+matteobonina96@users.noreply.github.com>
2020-12-07 23:48:15 +0100

    Update TeamRepository.java
    aggiunta @Repository

--- src/main/java/it/polito/ai/laboratorio3/repositories/TeamRepository.java
+++ src/main/java/it/polito/ai/laboratorio3/repositories/TeamRepository.java
@@ -2,6 +2,8 @@ package it.polito.ai.laboratorio3.repositories;
 
 import it.polito.ai.laboratorio3.entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends JpaRepository<Team,Long> {
 }
